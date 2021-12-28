4dd5ff4a7a52f2d8b89ea5ebd977e499025a75fc
Bruce Wayne <brucewayne@mail.com>
2021-12-28 13:49:10 -0500

    Create user and show items in market page

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
-from flask_wtf import FlaskForm
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm
 
 class RegisterForm(FlaskForm):
-	username = StringField(label='User Name:')
-	email = StringField(label='Email address:')
-	password1 = PasswordField(label="Password:")
-	password2 = PasswordField(label="Confirm password:")
+	username = StringField(label='User Name:', validators=[Length(min=2, max=50), DataRequired()])
+	email = StringField(label='Email address:', validators=[Email(), DataRequired()])
+	password1 = PasswordField(label="Password:", validators=[Length(min=6), DataRequired()])
+	password2 = PasswordField(label="Confirm password:", validators=[EqualTo('password1'), DataRequired()])
 	submit = SubmitField(label='Create Account')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, redirect, render_template
+from flask.helpers import url_for
 from app.forms import RegisterForm
+from app.models import Item, User
+from app import db
 
 views = Blueprint('views',__name__)
 
@@ -7,8 +10,28 @@ views = Blueprint('views',__name__)
 def index():
 	return render_template('index.html')
 
-@views.route('/register')
+@views.route('/market')
+def show_market():
+	items = Item.query.all()
+	return render_template('market.html', items=items)
+
+@views.route('/register', methods=['GET','POST'])
 def show_register():
 	form = RegisterForm()
+
+	if form.validate_on_submit():
+		usr = form.username.data
+		mail = form.email.data
+		passwd = form.password1.data
+		
+		new_user = User(username=usr, email=mail, password_hash=passwd)
+		db.session.add(new_user)
+		db.session.commit()
+		
+		return redirect(url_for('views.show_market'))
+
+	if form.errors != {}:
+		for err_msg in form.errors.values():
+			print(f'Validation form error: {err_msg}')
 	
 	return render_template('register.html', form=form)
